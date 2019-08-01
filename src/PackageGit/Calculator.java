823a8968d14b8e984be6803af197ba4d9399a81c
Ismailovajiba <ismailova.jibek@mail.ru>
2019-08-01 13:31:36 -0500

    Second commit

--- src/PackageGit/Calculator.java
+++ src/PackageGit/Calculator.java
@@ -4,4 +4,7 @@ public class Calculator {
     public int addition(int num1, int num2){
         return num1+num2;
     }
+    public int subtraction(int num1,int num2){
+        return num1-num2;
+    }
 }
